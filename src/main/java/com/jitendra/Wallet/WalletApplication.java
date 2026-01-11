0eb061b6dc1fe8ea65d696535eddf6ad2b3c297a
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-01-11 21:08:11 +0530

    Initial setup
    added sharding.yml file
    added config for sharding sphere
    updated application.properties

--- src/main/java/com/jitendra/Wallet/WalletApplication.java
+++ src/main/java/com/jitendra/Wallet/WalletApplication.java
@@ -1,5 +1,6 @@
 package com.jitendra.Wallet;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WalletApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Set environment variables
+		System.setProperty("POSTGRES_DB1_URL", dotenv.get("POSTGRES_DB1_URL"));
+		System.setProperty("POSTGRES_DB2_URL", dotenv.get("POSTGRES_DB2_URL"));
+		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+		System.setProperty("POSTGRES_PASS", dotenv.get("POSTGRES_PASS"));
+
 		SpringApplication.run(WalletApplication.class, args);
 	}
 
